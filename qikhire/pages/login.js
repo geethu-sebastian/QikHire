d435558d1e96b32b4bfd0e251c8068cc99e93ff4
Geethu Sebastian <geethugspdev@gmail.com>
2023-03-20 00:10:26 +0530

    login

--- qikhire/pages/login.js
+++ qikhire/pages/login.js
@@ -1,5 +1,7 @@
 import Head from "next/head"
 import Layout from "../layout/layout"
+import Link from "next/link"
+import styles from "../styles/Form.module.css"
 
 export default function Login(){
     return(
@@ -7,7 +9,41 @@ export default function Login(){
             <Head>
                 <title>Login</title>
             </Head>
-            <h1>Login</h1>
+            <section className="w-2/4 mx-auto flex flex-col gap-10">
+                <div className="title">
+                   <h1 className="text-black text-4xl font-bold py-4 ">QikHire</h1>
+                </div>
+                <form className="flex flex-col gap-5">
+                    <div className={styles.input_group}>
+                        <input type="email"
+                         name="email" 
+                         placeholder="Email"
+                         className={styles.input_text}
+                        />
+                    </div>
+                    <div className={styles.input_group}>
+                        <input type="password"
+                         name="password" 
+                         placeholder="password"
+                         className={styles.input_text}
+                        />
+                    </div>
+                    <div className="input-button">
+                        <button type="submit">
+                           Login
+                        </button>
+                    </div>
+                    <div className="input-button">   
+                        <button type="submit">
+                           Sign In with Google
+                        </button>
+                    </div>
+                    
+                </form>
+                <p className="text-center">
+                    New user? <Link href={'/register'}><a className="text-blue-700">Signup</a></Link>
+                </p>
+            </section>
         </Layout>
     )
 }
