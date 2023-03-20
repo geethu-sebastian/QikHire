e1a7a1d5536b6960d2ec13ffe9cac5eb0ed6f4b2
Geethu Sebastian <geethugspdev@gmail.com>
2023-03-20 20:26:10 +0530

    layout2 final

--- qikhire/layout/layout2.js
+++ qikhire/layout/layout2.js
@@ -10,13 +10,13 @@ export default function Layout2({children}){
                <div className=" flex title px-4 py-4 basis-1/5 text-start">
                 <h1 className="text-black text-4xl font-bold ">QikHire</h1>
                </div>
-               <div className=" basis-1/18 px-2 py-4 ">
-                  <Image src="profilef.svg" height={40} width={40} className=""/>
+               <div className=" basis-1/18 px-2 py-3 ">
+                  <Image src="profilef.svg" height={50} width={50} className=""/>
                </div>
 
             </div>
             <div className="flex flex-row basis-1/18 rounded-lg bg-slate-300 w-1/5 mx-8 my-2">
-              <div className="basis-1/6 bg-red-300 rounded-lg ">
+              <div className="basis-1/6 bg-slate-300 rounded-lg ">
 
               </div>
               <div className="">
